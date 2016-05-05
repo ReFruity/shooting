ba23a32490742374226823defcf7a88bd1944a5a
ReFruity <istomanton@gmail.com>
2016-05-05 20:38:14 +0500

    shooting method implemented

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
 from math import exp
 
-from numpy import arange
+
+class DiscreteFunction:
+    def __init__(self, a, h, ys):
+        self.a = a
+        self.b = a + h*(len(ys)-1)
+        self.h = h
+        self.points = []
+
+        for i in range(len(ys)):
+            self.points.append((a + i*h, ys[i]))
+
+    def __str__(self):
+        return ' '.join(map(str, self.points))
+
+    def get_points(self):
+        return self.points
+
+    def get_last_y(self):
+        return self.points[-1][1]
+
+    def raw_format(self):
+        return '\n'.join([str(y) for x, y in self.points])
 
 
 def main():
-    input_handle = open('input.txt', 'r')
     output_handle = open('output.txt', 'w')
 
+    a = 0
+    b = 5
     h = 0.1
-    x = 0
+    f1 = lambda x, y, z: z
+    f2 = lambda x, y, z: y + 3 + 1.5*x*x*(x-1)
+    g = lambda mu: f(output_handle, mu, f1, f2, a, b, h)
 
-    while x <= 5:
+    bisect_a = -100
+    bisect_b = 100
+    mu_res = bisect_method(g, bisect_a, bisect_b)
+
+    y_func, _ = cauchy_method(f1, f2, a, b, h, 3, mu_res)
+
+    print_value(output_handle, 'mu* = ' + str(mu_res))
+    print_value(output_handle, 'y(x, mu*) = \n' + y_func.raw_format())
+    # print_solution(output_handle, a, b, h)
+
+
+def f(output_handle, mu, f1, f2, a, b, h):
+    y_func, z_func = cauchy_method(f1, f2, a, b, h, 3, mu)
+
+    print_value(output_handle, y_func)
+    print_value(output_handle, z_func)
+
+    # y'(b) + y(b) - ...
+    result = z_func.get_last_y() + y_func.get_last_y() - 2 * exp(5) + 301.5
+
+    print_value(output_handle, result)
+    print_value(output_handle, '')
+
+    return result
+
+
+def print_value(output_handle, value):
+    print(str(value))
+    output_handle.write(str(value) + '\n')
+
+
+def print_solution(output_handle, a, b, h):
+    x = a
+
+    while x < b or close(x, b):
         y = solution(x)
 
         result = str(y)
@@ -31,19 +89,22 @@ def bisect_method(function, a, b):
     if function(b) == 0:
         return b
 
-    ab = b - a
     eps = 1e-8
+    ab2 = (b - a) / 2
+    c = a + ab2
+    f_c = function(c)
 
-    while b - a > eps:
-        ab /= 2
-        c = a + ab
-
-        if sign(function(a)) != sign(function(c)):
+    while abs(f_c) > eps:
+        if sign(function(a)) != sign(f_c):
             b = c
         else:
             a = c
 
-    return (a + b)/2
+        ab2 /= 2
+        c = a + ab2
+        f_c = function(c)
+
+    return (a + b) / 2
 
 
 def sign(x):
@@ -61,7 +122,8 @@ def cauchy_method(f1, f2, a, b, h, y0, z0):
     xn = a
     yn = y0
     zn = z0
-    result = [(a, y0)]
+    ys = [y0]
+    zs = [z0]
 
     while xn < b - h or close(xn, b - h):
         yn_temp = yn + h/2*f1(xn, yn, zn)
@@ -71,13 +133,14 @@ def cauchy_method(f1, f2, a, b, h, y0, z0):
         ynp1 = yn + h*f1(xn + h/2, yn_temp, zn_temp)
         znp1 = zn + h*f2(xn + h/2, yn_temp, zn_temp)
 
-        result.append((xnp1, ynp1))
+        ys.append(ynp1)
+        zs.append(znp1)
 
         xn = xnp1
         yn = ynp1
         zn = znp1
 
-    return result
+    return DiscreteFunction(a, h, ys), DiscreteFunction(a, h, zs)
 
 
 def test():
@@ -86,11 +149,9 @@ def test():
     a = 0
     b = 5
     h = 0.1
-    actual = cauchy_method(f1, f2, a, b, h, 3, 1)
-    actual_ys = [y for (x, y) in actual]
-    assert len(actual) == (b - a)/h + 1
-    expected_ys = [solution(x) for x in arange(a, b + h, h)]
-    assert list_distance(actual_ys, expected_ys) <= 1
+    y_func, z_func = cauchy_method(f1, f2, a, b, h, 3, 0)
+    assert len(y_func.points) == (b - a)/h + 1
+    assert len(z_func.points) == (b - a)/h + 1
 
     actual = bisect_method(lambda x: x, -1, 1)
     assert close(actual, 0)
@@ -99,16 +160,7 @@ def test():
     assert close(actual, 0.326289)
 
     actual = bisect_method(solution, 5, 6)
-    assert close(actual, 5.57278)
-
-
-def list_distance(list1, list2):
-    result = 0
-
-    for i in range(min(len(list1), len(list2))):
-        result += abs(list1[i] - list2[i])
-
-    return result
+    assert close(actual, 5.572782)
 
 
 def close(a, b):
